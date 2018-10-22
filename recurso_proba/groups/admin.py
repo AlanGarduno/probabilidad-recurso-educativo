ff4b135cf1f28f3de7c72cb7d43cc8c6c922f08b
AlanGarduno <agv_lrd@hotmail.com>
2018-10-22 08:50:54 -0500

    Actualizacion

--- recurso_proba/groups/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-class GroupMemberInline(admin.TabularInline):
-    '''Tabular Inline View for GroupMember '''
-    model = models.GroupMember
-
-admin.site.register(models.Group)
